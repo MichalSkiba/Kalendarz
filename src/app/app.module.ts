580cf36185c0b9c05aa91ab48b52b3984fece209
MichalSkiba <michalskiba.ms@gmail.com>
2019-05-27 18:19:25 +0200

    Kalendarz do poprawy

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
+  ],
+  exports: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
